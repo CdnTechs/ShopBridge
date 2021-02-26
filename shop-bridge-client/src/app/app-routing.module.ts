958caee06b01d2208bd399b93433bc423490de81
Chandan Kumar <chandan.k@ehealthflex.com>
2021-02-27 00:13:20 +0530

    shop component

--- shop-bridge-client/src/app/app-routing.module.ts
+++ shop-bridge-client/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { ShopComponent } from './shop/shop.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'shop', component: ShopComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: '**', redirectTo: '', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- shop-bridge-client/src/app/app.module.ts
+++ shop-bridge-client/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { ShopComponent } from './shop/shop.component';
 import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ShopComponent,
-    HomeComponent
+    HomeComponent,
+    NavBarComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
